bc330edcab3663790f33508796af8d82342f945f
Natasha Murashev <nmurashev@gmail.com>
2011-10-30 17:33:30 -0700

    Getting the folders straight. Now, you can access the Defend Democracy Karel solution :)

--- Assignment1/defendDemocracyKarel.java
+++ Assignment1/defendDemocracyKarel.java
@@ -1,16 +1,63 @@
-import stanford.karel.*;
+/* Karel is cleaning up the voting ballot. 
+ * The vote only counts if the middle rectangle is punched,
+ * which means there are no beepers present. 
+ */
+
+import stanford.karel.SuperKarel;
 
 public class defendDemocracyKarel extends SuperKarel {
 
 public void run() {
 	while (frontIsClear()) {
 		move();
-		checkBallot();
+		checkMiddleBeeper();
+		cleanExtraBeepers();
 		move();
 	}
 }
-private void checkBallot() {
-	turnRight();
+private void checkMiddleBeeper() {
+	if (beepersPresent()) {
+		move();
+		move();
+	}
 }
-	
+private void cleanExtraBeepers() {
+	if (noBeepersPresent()) {
+		cleanBeepersSouth();
+		cleanBeepersNorth();
+	}
+}
+/* Karel moves South, checks for beepers, and picks them up
+ * if there are any there. 
+ * Pre-condition: Karel is in the middle of a rectangle with no 
+ * middle beeper
+ * Post-condition: Karel returns to middle facing North
+ */
+private void cleanBeepersSouth() {
+	turnRight();
+	move();
+	while (beepersPresent()) {
+		pickBeeper(); 
+	}
+	turnAround();
+	move();
+}
+/* Karel moves North, checks for beepers, and picks them up
+ * if there are any there. 
+ * Pre-condition: Karel is in the middle of a rectangle with no 
+ * middle beeper
+ * Post-condition: Karel returns to middle facing East
+ */
+private void cleanBeepersNorth() {
+	move();
+	if (frontIsClear()) {
+		move();
+	}
+	while (beepersPresent()) {
+		pickBeeper();
+	}
+	turnAround();
+	move();
+	turnLeft();
+	}
 }
