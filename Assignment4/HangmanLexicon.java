8a77cf627fe9b617bff44299bbf274705cc743d2
Natasha Murashev <nmurashev@gmail.com>
2011-11-17 14:50:35 -0800

    Revert f4dd7dae19eaf534d5cb74011a5a7d2ef8f8c5cd^..HEAD

--- Assignment4/HangmanLexicon.java
+++ Assignment4/HangmanLexicon.java
@@ -11,7 +11,6 @@ import java.util.*;
 
 public class HangmanLexicon {
 
-<<<<<<< HEAD
 	private ArrayList <String> wordList = new ArrayList <String> ();	
 
 	public HangmanLexicon() {
@@ -28,33 +27,13 @@ public class HangmanLexicon {
 			throw new ErrorException(ex);
 		}
 	}
-=======
-private ArrayList <String> wordList = new ArrayList <String> ();	
-
-public HangmanLexicon() {
-	//adds the individual words in the file to the array list
-	try {
-		BufferedReader hangmanWords = new BufferedReader(new FileReader("HangmanLexicon.txt"));
-		while(true) {
-			String line = hangmanWords.readLine();
-			if(line == null) break;
-			wordList.add(line);
-		}
-		hangmanWords.close();
-	} catch (IOException ex) {
-		throw new ErrorException(ex);
-	}
-}
->>>>>>> parent of 3008681... Deleted Files
 
 /** Returns the word at the specified index. */
 	public String getWord(int index) {
 		return wordList.get(index);
-<<<<<<< HEAD
+
 	}
-=======
-}
->>>>>>> parent of 3008681... Deleted Files
+
 	
 	/** Returns the number of words in the lexicon. */
 	public int getWordCount() {
