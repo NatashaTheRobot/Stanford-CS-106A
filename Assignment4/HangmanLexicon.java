aecf7ed7f0496bf7ce54e59e473809f7539ad04c
Natasha Murashev <nmurashev@gmail.com>
2011-11-17 14:25:35 -0800

    Stanford CS106A HangmanLexicon Solution

--- Assignment4/HangmanLexicon.java
+++ Assignment4/HangmanLexicon.java
@@ -6,6 +6,7 @@
  */
 
 import acm.util.*;
+<<<<<<< HEAD
 
 public class HangmanLexicon {
 
@@ -30,4 +31,37 @@ public class HangmanLexicon {
 			default: throw new ErrorException("getWord: Illegal index");
 		}
 	};
+=======
+import java.io.*;
+import java.util.*;
+
+public class HangmanLexicon {
+
+private ArrayList <String> wordList = new ArrayList <String> ();	
+
+public HangmanLexicon() {
+	//adds the individual words in the file to the array list
+	try {
+		BufferedReader hangmanWords = new BufferedReader(new FileReader("HangmanLexicon.txt"));
+		while(true) {
+			String line = hangmanWords.readLine();
+			if(line == null) break;
+			wordList.add(line);
+		}
+		hangmanWords.close();
+	} catch (IOException ex) {
+		throw new ErrorException(ex);
+	}
+}
+
+/** Returns the word at the specified index. */
+	public String getWord(int index) {
+		return wordList.get(index);
+}
+	
+	/** Returns the number of words in the lexicon. */
+	public int getWordCount() {
+		return wordList.size();
+	}	
+>>>>>>> parent of 3008681... Deleted Files
 }
